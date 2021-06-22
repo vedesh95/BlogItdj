e02223864d96435fffb1cbc3802f9327531d74e1
vedesh95 <vedesh95@gmail.com>
2021-06-22 14:48:40 +0530

    fixed mobile view

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('admin/', admin.site.urls),
     path('contact',views.contact,name="contact"),
-    path('about',views.about,name="about"),
     path('signup',views.signup,name="signup"),
     path('login',views.handlelogin,name="handlelogin"),
     path('logout',views.handlelogout,name="handlelogout"),
--- project/views.py
+++ project/views.py
@@ -9,8 +9,6 @@ def home(request):
     context={"posts":posts}
     return render(request,"index.html",context)
 
-def about(request):
-    return render(request,"about.html")
 def signup(request):
     if request.method=="POST":
         username=request.POST.get('username')
